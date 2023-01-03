66894fdbcd6356e0c887e1447272d71020cdfd3e
Agnes K. Rivers <agneskrivers@gmail.com>
2023-01-03 20:17:58 +0700

    Config Babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				extensions: [
+					'.ios.js',
+					'.android.js',
+					'.js',
+					'.ts',
+					'.tsx',
+					'.json',
+				],
+				alias: {
+					'@assets': './assets',
+					'@components': './src/components',
+					'@configs': './src/configs',
+					'@context': './src/context',
+					'@helpers': './src/helpers',
+					'@navigation': './src/navigation',
+					'@screens': './src/screens',
+					'@utils': './src/utils',
+				},
+			},
+		],
+	],
 };
