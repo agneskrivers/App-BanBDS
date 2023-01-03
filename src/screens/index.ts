be2bb282fe6dc4f3252087c2255a019f53514498
Agnes K. Rivers <agneskrivers@gmail.com>
2023-01-03 20:29:29 +0700

    Screens

---
+++ src/screens/index.ts
@@ -0,0 +1,17 @@
+export { default as AccountScreen } from './Account';
+export { default as ConfirmScreen } from './Confirm';
+export { default as EditPostScreen } from './EditPost';
+export { default as FormScreen } from './Form';
+export { default as HomeScreen } from './Home';
+export { default as LoginScreen } from './Login';
+export { default as MyPostScreen } from './MyPost';
+export { default as MyPostsScreen } from './MyPosts';
+export { default as NewsScreen } from './News';
+export { default as NewsInfoScreen } from './NewsInfo';
+export { default as PostScreen } from './Post';
+export { default as PostsScreen } from './Posts';
+export { default as ProfileScreen } from './Profile';
+export { default as ProjectScreen } from './Project';
+export { default as ProjectsScreen } from './Projects';
+export { default as UserScreen } from './User';
+export { default as ZoningScreen } from './Zoning';
