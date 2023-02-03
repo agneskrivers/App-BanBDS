a5bf4bc46bb6317bc99bd9e765fa60448fd5c949
Agnes K. Rivers <agneskrivers@gmail.com>
2023-02-03 11:19:19 +0700

    Add Helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,8 @@
+export { default as convertToEnglish } from './convertToEnglish';
 export { default as formatPhoneNumber } from './formatPhoneNumber';
 export { default as formatTime } from './formatTime';
 export { default as getName } from './getName';
 export { default as getPricePerAcreage } from './getPricePerAcreage';
 export { default as isValidPhoneNumber } from './isValidPhoneNumber';
+export { default as renewTokenDevice } from './renewTokenDevice';
+export { default as storages } from './storages';
